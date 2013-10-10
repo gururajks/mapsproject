ef4a606d0ec1972c99fc798a16bf71d90efa7796
gururajks <gururaj.genius@gmail.com>
2013-10-10 10:29:28 +0530

    Maps working

--- gen/com/gis/ngsmapsproject/BuildConfig.java
+++ gen/com/gis/ngsmapsproject/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.ngsmapsproject;
+package com.gis.ngsmapsproject;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- src/com/gis/ngsmapsproject/HomeActivity.java
+++ src/com/gis/ngsmapsproject/HomeActivity.java
@@ -1,17 +1,40 @@
 package com.gis.ngsmapsproject;
 
-import com.example.ngsmapsproject.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.widget.TextView;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends FragmentActivity {
+	
+	GoogleMap gMapFragment;
+	private final String GPS_MENU_KEY = "gps_setting";
+	boolean gps_menu_setting;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		
+		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+		if(gMapFragment == null) {
+	    	gMapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+	    	if(gMapFragment != null) {
+	    		gMapFragment.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+	    		gps_menu_setting = sharedPref.getBoolean(GPS_MENU_KEY, true);	    			    		
+	    		gMapFragment.setMyLocationEnabled(gps_menu_setting);	      		
+	    	}
+	    }	 
+		
 	}
 
 	@Override
